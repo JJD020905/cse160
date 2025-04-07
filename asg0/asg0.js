ed340e4b1646d109c42ae2df31b2b813b91e0fdc
Junjie Deng <jdeng42@ucsc.edu>
2025-04-06 20:48:44 -0700

    Initial commit

---
+++ asg0/asg0.js
@@ -0,0 +1,16 @@
+   // DrawRectangle.js
+   function main() {
+    // Retrieve <canvas> element                                  <- (1)
+     var canvas = document.getElementById('example');
+     if (!canvas) {
+       console.log('Failed to retrieve the <canvas> element');
+       return;
+     }
+
+   // Get the rendering context for 2DCG                          <- (2)
+   var ctx = canvas.getContext('2d');
+
+   // Draw a blue rectangle                                       <- (3)
+   ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a blue color
+   ctx.fillRect(0, 0, canvas.width, canvas.height); // Fill a rectangle with the color
+   }
